7c77aebe7275b669ae7387445bc85a0c04bef141
Carsten Hagemann <chageman@au1.ibm.com>
2025-12-01 15:03:06 +1000

    Add authorization code flow demo app

---
+++ examples/authcodeflow_demo/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.compose.compiler)
+}
+
+apply(from = "$rootDir/common-config-demos.gradle")
+apply(from = "$rootDir/common-config-ktor.gradle")
+
+android {
+    namespace = "com.ibm.security.verifysdk.authcodeflow.demoapp"
+    defaultConfig {
+        applicationId = "com.ibm.security.verifysdk.authcodeflow.demoapp"
+
+        manifestPlaceholders["auth_redirect_scheme"] = "verifysdk"
+        manifestPlaceholders["auth_redirect_host"] = "callback"
+        manifestPlaceholders["auth_redirect_path"] = "/redirect"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(project(":sdk:authentication"))
+
+    implementation(libs.zxing.android.embedded)
+    implementation(libs.androidx.biometric)
+
+    // Compose dependencies
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    androidTestImplementation(libs.androidx.espresso.core)
+}
