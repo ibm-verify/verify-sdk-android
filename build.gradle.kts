63f12c39a8f131521c07b591d3b11dc7fad6dc13
Carsten Hagemann <chageman@au1.ibm.com>
2026-06-01 22:46:27 +1000

    chore: Bump version to 3.2.4 (code 122)
    - Updated version from 3.2.3 to 3.2.4
    - Temporarily disabled Dokka documentation generation
    - Version code incremented to 122

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
-    alias(libs.plugins.jetbrains.dokka)
+//    alias(libs.plugins.jetbrains.dokka)
     java
     jacoco
     alias(libs.plugins.neotech.rootcoverage) apply true
@@ -15,8 +15,8 @@ plugins {
 }
 
 // used for release naming and in MFA SDK
-extra["versionName"] = "3.2.3"
-extra["versionCode"] = "121"
+extra["versionName"] = "3.2.4"
+extra["versionCode"] = "122"
 
 dependencies {
     add("implementation", enforcedPlatform("com.fasterxml.jackson:jackson-bom:2.15.3"))
@@ -110,7 +110,7 @@ tasks.jacocoTestCoverageVerification {
 subprojects {
     apply {
         plugin("maven-publish")
-        plugin("org.jetbrains.dokka")
+//        plugin("org.jetbrains.dokka")
     }
 
     /**
